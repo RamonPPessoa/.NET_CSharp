fee341693de59c13f2a6685f9deed7f331ce8588
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-02-11 18:56:01 -0400

    Realizando testes de unidade em codigos C# utilizando MSTest

---
+++ Testes/LearnMyCalculatorApp/LearnMyCalculatorApp.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LearnMyCalculatorApp;
+namespace LearnMyCalculatorApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CalculatorNullTest()
+        {
+            var calculator = new Calculator();
+            Assert.IsNotNull(calculator);
+            Assert.IsTrue(false); // Will fail the test
+        }
+    }
+}
