486cee5c9d72952733562c5aec5032d0f436b482
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-02-28 18:31:11 -0400

    Criando a controller da API,para realizar as ações

---
+++ API_Pizzaria/Controllers/PizzaController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using API_Pizzaria.Models;
+using API_Pizzaria.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Pizzaria.Controllers
+{
+    [ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase
+{
+    public PizzaController()
+    {
+    }
+
+    // GET all action
+
+[HttpGet]
+public ActionResult<List<Pizza>> GetAll() =>
+PizzaService.GetAll();
+
+    // GET by Id action
+
+[HttpGet("{id}")]
+public ActionResult<Pizza> Get(int id)
+{
+    var pizza = PizzaService.Get(id);
+
+    if(pizza == null)
+        return NotFound();
+
+    return pizza;
+}
+
+    // POST action
+
+    // PUT action
+
+    // DELETE action
+}
+}
