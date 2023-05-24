2fd17d9b0f84e954a2633093d1257ad1f3846a6a
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-05-24 10:04:16 -0400

    Programação Orientada a Objetos

---
+++ POO/Aluno.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    public class Aluno
+    {
+        public string? Nome;
+        public int Idade;
+        public int Serie;
+
+public string Retorno()
+{
+return string.Format($"O aluno {Nome} tem {Idade} anos e pertence ao {Serie} ano");
+}
+public void RetornoConsole()
+{
+Console.WriteLine(Retorno());
+    
+    
+}
+    }
+}
---
+++ POO/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using POO;
+
+Aluno aluno1 = new Aluno(); 
+aluno1.Nome = "João"; 
+aluno1.Idade = 13; 
+aluno1.Serie = 6;
+
+aluno1.RetornoConsole();
