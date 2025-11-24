660eb6b4e9d735d981321e51fa64d4c84f19984b
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2025-11-24 18:20:44 -0400

    Renomeando o arquivo sln

--- TrabalhandoCSharpDotNET/EstudandoCSharp/obj/Debug/net9.0/EstudandoCSharp.AssemblyInfo.cs
+++ TrabalhandoCSharpDotNET/EstudandoCSharp/obj/Debug/net9.0/EstudandoCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EstudandoCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0896b0230732f0f41079462f78cd95b46f2c6f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8d1f0395cbe4e8453c540ad2575e0b68bd6641c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EstudandoCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EstudandoCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
